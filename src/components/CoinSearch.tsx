3736e120ae5ab5cc36c6216e514051e89dc60712
Umang Agarwal <umangagarwal257@gmail.com>
2025-05-19 14:50:28 +0530

    feat: enhance CoinSearch component with search functionality and improved styling

--- src/components/CoinSearch.tsx
+++ src/components/CoinSearch.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import type { Coin } from "../types/Coin";
 import { CoinItem } from "./CoinItem"
 
@@ -6,32 +7,44 @@ interface CoinSearchProps {
 }
 
 export const CoinSearch = ({coins} : CoinSearchProps) => {
-    console.log(coins)
+    const[searchText, setSearchText] = useState<string>("")
+
   return (
-    <div>
-        <div>
-            <h1>Search Crypto</h1>
+    <div className="rounded-div my-4">
+        <div className="flex flex-col md:flex-row text-center justify-between pt-4 pb-6 md:text-right">
+            <h1 className="text-2xl font-bold my-2">Search Crypto</h1>
             <form>
-                <input type="text" placeholder="Search a coin" className="w-full h-10 p-2 bg-primary text-accent border border-secondary rounded-2xl shadow-xl"/>
+                <input 
+                onChange={(e) => setSearchText(e.target.value)} 
+                className="w-full px-4 py-2 bg-primary text-accent border border-input rounded-2xl shadow-xl"
+                type="text" 
+                placeholder="Search a coin" 
+                />
             </form>
         </div>
 
-        <table>
+        <table className="w-full border-collapse text-center">
             <thead>
                 <tr>
                     <th></th>
-                    <th>#</th>
-                    <th>Coin</th>
+                    <th className="px-4">#</th>
+                    <th className="text-left">Coin</th>
                     <th></th>
                     <th>Price</th>
                     <th>24h Change</th>
-                    <th>24h Volume</th>
-                    <th>Market Cap</th>
+                    <th className="hidden md:table-cell">24h Volume</th>
+                    <th className="hidden md:table-cell">Market Cap</th>
                     <th>Last 7 Days</th>
                 </tr>
             </thead>
             <tbody>
-                {coins.map((coin) => (
+                {coins.filter((value) => {
+                    if(searchText === "") {
+                        return value
+                    } else if(value.name.toLowerCase().includes(searchText.toLowerCase())) {
+                        return value
+                    }
+                }).map((coin) => (
                     <CoinItem key={coin.id} coin={coin}/>
                 ))}
             </tbody>
